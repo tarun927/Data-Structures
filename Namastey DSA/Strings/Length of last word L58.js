469512b31ff807e940296d10856f34d340670881
tarun927 <82480762+tarun927@users.noreply.github.com>
2026-08-09 15:23:23 +0530

    Add function to calculate length of last word

---
+++ Namastey DSA/Strings/Length of last word L58.js
@@ -0,0 +1,11 @@
+var lengthOfLastWord = function(s) {
+    let count = 0;
+    for(let i=s.length-1; i>=0; i--){
+        if(s[i] === " "){
+           if(count !== 0) break;
+        }else{
+            count++;
+        }
+    }
+    return count;
+};
